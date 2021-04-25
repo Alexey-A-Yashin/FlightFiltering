2eaab4065a397c60e6a52eb99dc26256aa99f9a8
Alexey A. Yashin <alexey.a.yashin@gmail.com>
2021-04-25 14:16:05 +0300

    Добавьте файлы проекта.

---
+++ FlightFiltering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlightFiltering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
