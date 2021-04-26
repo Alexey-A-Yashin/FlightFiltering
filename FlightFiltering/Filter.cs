80599d7a4afbe4832e1750cce56a4e12eb508ea6
Alexey A. Yashin <alexey.a.yashin@gmail.com>
2021-04-26 04:21:27 +0300

    Version 1

---
+++ FlightFiltering/Filter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gridnine.FlightCodingTest
+{
+    public static class FlightFilter
+    {
+        public static IList<Flight> GetFlightsWithDepartureUntilNow(this IList<Flight> flights)
+        {
+            return flights.SelectMany(f => f.Segments, (f, s) => new { Flight = f, Segment = s })
+                    .Where(fs => fs.Segment.DepartureDate <= DateTime.Now)
+                    .Select(fs => fs.Flight)
+                    .Distinct()
+                    .ToList();
+        }
+
+        public static IList<Flight> GetFlightsWithArrivalEarlierDeparture(this IList<Flight> flights)
+        {
+            return flights.SelectMany(f => f.Segments, (f, s) => new { Flight = f, Segment = s })
+                    .Where(fs => fs.Segment.ArrivalDate < fs.Segment.DepartureDate)
+                    .Select(fs => fs.Flight)
+                    .Distinct()
+                    .ToList();
+        }
+
+        public static IList<Flight> GetTimedFlightsOnTheGround(this IList<Flight> flights, double time)
+        {
+            var flightSegments = flights.SelectMany(f => f.Segments, (f, s) => new { Flight = f, DepartureDate = s.DepartureDate, ArrivalDate = s.ArrivalDate }).ToList();
+            return flightSegments.Zip(flightSegments.Skip(1), (currentSegment, nextSegment)
+                    => new { currentSegment.Flight,  
+                            TimeOnTheGround = (currentSegment.Flight == nextSegment.Flight) ? nextSegment.DepartureDate.Subtract(currentSegment.ArrivalDate) : TimeSpan.Zero})
+                    .GroupBy(ft => ft.Flight).Select(ft => new { Flight = ft.Key, TimeOnTheGround = ft.Sum(ft => ft.TimeOnTheGround.TotalHours)})
+                    .Where(ft => ft.TimeOnTheGround > time)
+                    .Select(fs => fs.Flight)
+                    .ToList();
+        }
+    }
+}
--- FlightFiltering/Program.cs
+++ FlightFiltering/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace FlightFiltering
+namespace Gridnine.FlightCodingTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FlightBuilder flightBuilder = new FlightBuilder();
+            IList<Flight> flights = flightBuilder.GetFlights();
+
+            ShowMessege("All flights:", ConsoleColor.Red);
+            foreach (Flight flight in flights)
+                ShowMessege(flight.ToString(), ConsoleColor.Yellow);
+
+            ShowMessege("Flights with departure date up to the current time are excluded:", ConsoleColor.Red);
+            foreach (Flight flight in flights.Except(flights.GetFlightsWithDepartureUntilNow()))
+                ShowMessege(flight.ToString(), ConsoleColor.Yellow);
+
+            ShowMessege("Flights with an arrival date earlier than the departure date are excluded:", ConsoleColor.Red);
+            foreach (Flight flight in flights.Except(flights.GetFlightsWithArrivalEarlierDeparture()))
+                ShowMessege(flight.ToString(), ConsoleColor.Yellow);
+
+            ShowMessege("flights with time on the ground exceeding 2 hours:", ConsoleColor.Red);
+            foreach (Flight flight in flights.GetTimedFlightsOnTheGround(2))
+                ShowMessege(flight.ToString(), ConsoleColor.Yellow);
+            
+            Console.ReadLine();
+        }
+
+        public static void ShowMessege(string message, ConsoleColor color)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = currentColor;
         }
     }
 }
